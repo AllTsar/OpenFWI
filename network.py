35a87bb3044f01fbe92adbb21a11fb5bc0208867
AllTsar <journeywinkle@yahoo.com>
2026-03-12 13:45:40 -0500

    Implemented InversionNetCBAM

--- network.py
+++ network.py
@@ -128,6 +128,124 @@ class ResizeBlock(nn.Module):
     def forward(self, x):
         return self.layers(x)
 
+########################## CBAM building blocks #################################################################
+
+class ChannelAttention(nn.Module):
+    def __init__(self, in_channels, reduction=16):
+        super().__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.max_pool = nn.AdaptiveMaxPool2d(1)
+        self.mlp = nn.Sequential(
+            nn.Conv2d(in_channels, in_channels // reduction, 1, bias=False),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels // reduction, in_channels, 1, bias=False)
+        )
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        avg_out = self.mlp(self.avg_pool(x))
+        max_out = self.mlp(self.max_pool(x))
+        return self.sigmoid(avg_out + max_out)
+
+class SpatialAttention(nn.Module):
+    def __init__(self, kernel_size=7):
+        super().__init__()
+        self.conv = nn.Conv2d(2, 1, kernel_size, padding=kernel_size//2, bias=False)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        avg_out = torch.mean(x, dim=1, keepdim=True)
+        max_out, _ = torch.max(x, dim=1, keepdim=True)
+        x_cat = torch.cat([avg_out, max_out], dim=1)
+        return self.sigmoid(self.conv(x_cat))
+
+
+class CBAM(nn.Module):
+    def __init__(self, in_channels, reduction=16, kernel_size=7):
+        super().__init__()
+        self.ca = ChannelAttention(in_channels, reduction)
+        self.sa = SpatialAttention(kernel_size)
+
+    def forward(self, x):
+        x = x * self.ca(x)
+        x = x * self.sa(x)
+        return x
+
+
+### New InversionNet with just CBAMs added
+class InversionNetCBAM(nn.Module):
+    def __init__(self, dim1=32, dim2=64, dim3=128, dim4=256, dim5=512, sample_spatial=1.0, **kwargs):
+        super(InversionNetCBAM, self).__init__()
+        self.convblock1 = ConvBlock(5, dim1, kernel_size=(7, 1), stride=(2, 1), padding=(3, 0))
+        self.convblock2_1 = ConvBlock(dim1, dim2, kernel_size=(3, 1), stride=(2, 1), padding=(1, 0))
+        self.convblock2_2 = ConvBlock(dim2, dim2, kernel_size=(3, 1), padding=(1, 0))
+        self.convblock3_1 = ConvBlock(dim2, dim2, kernel_size=(3, 1), stride=(2, 1), padding=(1, 0))
+        self.convblock3_2 = ConvBlock(dim2, dim2, kernel_size=(3, 1), padding=(1, 0))
+        self.convblock4_1 = ConvBlock(dim2, dim3, kernel_size=(3, 1), stride=(2, 1), padding=(1, 0))
+        self.convblock4_2 = ConvBlock(dim3, dim3, kernel_size=(3, 1), padding=(1, 0))
+        self.convblock5_1 = ConvBlock(dim3, dim3, stride=2)
+        self.convblock5_2 = ConvBlock(dim3, dim3)
+        self.cbam_enc5 = CBAM(dim3, reduction=8)
+        self.convblock6_1 = ConvBlock(dim3, dim4, stride=2)
+        self.convblock6_2 = ConvBlock(dim4, dim4)
+        self.cbam_enc6 = CBAM(dim4, reduction=16)
+        self.convblock7_1 = ConvBlock(dim4, dim4, stride=2)
+        self.convblock7_2 = ConvBlock(dim4, dim4)
+        self.cbam_enc7 = CBAM(dim4, reduction=16)
+        self.convblock8 = ConvBlock(dim4, dim5, kernel_size=(8, ceil(70 * sample_spatial / 8)), padding=0)
+
+        self.deconv1_1 = DeconvBlock(dim5, dim5, kernel_size=5)
+        self.deconv1_2 = ConvBlock(dim5, dim5)
+        self.cbam_dec1=CBAM(dim5, reduction=16)
+        self.deconv2_1 = DeconvBlock(dim5, dim4, kernel_size=4, stride=2, padding=1)
+        self.deconv2_2 = ConvBlock(dim4, dim4)
+        self.cbam_dec2 = CBAM(dim4, reduction=16)
+        self.deconv3_1 = DeconvBlock(dim4, dim3, kernel_size=4, stride=2, padding=1)
+        self.deconv3_2 = ConvBlock(dim3, dim3)
+        self.cbam_dec3 = CBAM(dim3, reduction=8)
+        self.deconv4_1 = DeconvBlock(dim3, dim2, kernel_size=4, stride=2, padding=1)
+        self.deconv4_2 = ConvBlock(dim2, dim2)
+        self.deconv5_1 = DeconvBlock(dim2, dim1, kernel_size=4, stride=2, padding=1)
+        self.deconv5_2 = ConvBlock(dim1, dim1)
+        self.deconv6 = ConvBlock_Tanh(dim1, 1)
+
+    def forward(self, x):
+        # Encoder Part
+        x = self.convblock1(x)  # (None, 32, 500, 70)
+        x = self.convblock2_1(x)  # (None, 64, 250, 70)
+        x = self.convblock2_2(x)  # (None, 64, 250, 70)
+        x = self.convblock3_1(x)  # (None, 64, 125, 70)
+        x = self.convblock3_2(x)  # (None, 64, 125, 70)
+        x = self.convblock4_1(x)  # (None, 128, 63, 70)
+        x = self.convblock4_2(x)  # (None, 128, 63, 70)
+        x = self.convblock5_1(x)  # (None, 128, 32, 35)
+        x = self.convblock5_2(x)  # (None, 128, 32, 35)
+        x = self.cbam_enc5(x)
+        x = self.convblock6_1(x)  # (None, 256, 16, 18)
+        x = self.convblock6_2(x)  # (None, 256, 16, 18)
+        x = self.cbam_enc6(x)
+        x = self.convblock7_1(x)  # (None, 256, 8, 9)
+        x = self.convblock7_2(x)  # (None, 256, 8, 9)
+        x = self.cbam_enc7(x)
+        x = self.convblock8(x)  # (None, 512, 1, 1)
+
+        # Decoder Part
+        x = self.deconv1_1(x)  # (None, 512, 5, 5)
+        x = self.deconv1_2(x)  # (None, 512, 5, 5)
+        x = self.cbam_dec1(x)
+        x = self.deconv2_1(x)  # (None, 256, 10, 10)
+        x = self.deconv2_2(x)  # (None, 256, 10, 10)
+        x = self.cbam_dec2(x)
+        x = self.deconv3_1(x)  # (None, 128, 20, 20)
+        x = self.deconv3_2(x)  # (None, 128, 20, 20)
+        x = self.cbam_dec3(x)
+        x = self.deconv4_1(x)  # (None, 64, 40, 40)
+        x = self.deconv4_2(x)  # (None, 64, 40, 40)
+        x = self.deconv5_1(x)  # (None, 32, 80, 80)
+        x = self.deconv5_2(x)  # (None, 32, 80, 80)
+        x = F.pad(x, [-5, -5, -5, -5], mode="constant", value=0)  # (None, 32, 70, 70) 125, 100
+        x = self.deconv6(x)  # (None, 1, 70, 70)
+        return x
 
 
 # FlatFault/CurveFault
@@ -316,6 +434,7 @@ class Deconv_HPGNN(nn.Module):
 model_dict = {
     'InversionNet': InversionNet,
     'Discriminator': Discriminator,
-    'UPFWI': FCN4_Deep_Resize_2
+    'UPFWI': FCN4_Deep_Resize_2,
+    'InversionNetCBAM': InversionNetCBAM
 }
 
